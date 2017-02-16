155503477d07198ce39380748b9a4c1b96339b61
Sumit Mukhija <sumitmukhija@hotmail.com>
2017-02-16 10:42:30 +0530

    Shadow and corner radius

--- Cards/PrimaryView.swift
+++ Cards/PrimaryView.swift
@@ -10,4 +10,14 @@ import UIKit
 
 class PrimaryView: UIView {
     
+    override func awakeFromNib() {
+        self.layer.cornerRadius = 10.0
+        self.layer.borderWidth = 0.6
+        self.layer.borderColor = UIColor.lightGrayColor().CGColor
+        self.layer.shadowColor = UIColor.blackColor().CGColor
+        self.layer.shadowOffset = CGSizeMake(0.3, 3.0);
+        self.layer.shadowOpacity = 0.3
+        self.layer.shadowRadius = 3.0
+    }
+    
 }
