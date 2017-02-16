0cfd77dc2c9d5edcf8183a1bb8cc01c975be729b
Sumit Mukhija <sumitmukhija@hotmail.com>
2017-02-16 10:34:40 +0530

    Added Primary View

---
+++ Cards/PrimaryView.swift
@@ -0,0 +1,13 @@
+//
+//  PrimaryView.swift
+//  Cards
+//
+//  Created by Sumit on 2/16/17.
+//  Copyright © 2017 Sumit Mukhija. All rights reserved.
+//
+
+import UIKit
+
+class PrimaryView: UIView {
+    
+}
